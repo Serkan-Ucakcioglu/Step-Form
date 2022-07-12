b26101aca8ddea7caf09db8eb608c1437eba577a
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2022-07-13 01:51:29 +0300

    indent syntax

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,38 +1,33 @@
-import { InjectionKey } from 'vue'
-import { createStore, useStore as baseUseStore, Store } from 'vuex'
+import { InjectionKey } from "vue";
+import { createStore, useStore as baseUseStore, Store } from "vuex";
 import createPersistedState from "vuex-persistedstate";
 
-
 export interface State {
-  email: string,
-  password: string,
-  select: string,
-  age: number,
-  count: number,
-  name: string,
-  surname: string
-
+  email: string;
+  password: string;
+  select: string;
+  age: number;
+  count: number;
+  name: string;
+  surname: string;
 }
 
-
-
-export const key: InjectionKey<Store<State>> = Symbol()
+export const key: InjectionKey<Store<State>> = Symbol();
 
 export const store = createStore<State>({
   state: {
-    email: '',
-    password: '',
-    select: '',
+    email: "",
+    password: "",
+    select: "",
     age: 0,
     count: 0,
-    name: '',
-    surname: ''
+    name: "",
+    surname: "",
   },
-  plugins: [createPersistedState()]
-  
-})
+  plugins: [createPersistedState()],
+});
 
 // define your own `useStore` composition function
-export function useStore () {
-  return baseUseStore(key)
+export function useStore() {
+  return baseUseStore(key);
 }
